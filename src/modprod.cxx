f1c741bcaf83e9f07fade9f9a81f05382a5da299
Marcus Luebke <marcusxluebke@gmail.com>
2021-04-23 15:15:28 -0700

    Pass by reference didn't work
    I tried creating a mutable constexpr function using pass by reference
    Constexpr said no

--- src/modprod.cxx
+++ src/modprod.cxx
@@ -1,15 +1,45 @@
 #pragma once
 #include <limits>
+#include <cstdint>
+
+#define size_t uint64_t
+#define times *
+
+constexpr int i = 0xFFFF;
+constexpr unsigned char c = i;
+constexpr unsigned char halfsize{sizeof(size_t) * 4};
 
 struct u128 {
 	size_t greater;
 	size_t lesser;
+	constexpr u128(size_t greater, size_t lesser) noexcept:
+		greater{greater},
+		lesser{lesser}
+	{}
 };
 
-size_t MAX = std::numeric_limits<size_t>::max();
+struct u32_2 {
+	static constexpr size_t lesser_mask32 = 0x00000000FFFFFFFF;
+	size_t greater;
+	size_t lesser;
+	constexpr u32_2(size_t a) noexcept:
+		greater{a >> halfsize},
+		lesser{a & u32_2::lesser_mask32}
+	{}
+};
 
-bool add_will_overflow(size_t a, size_t b) {
+constexpr size_t MAX = std::numeric_limits<size_t>::max();
+
+constexpr bool add_will_overflow(size_t a, size_t b) {
 	return b > MAX - a;
+}
+
+template<u128 *a, u128 *b>
+constexpr void addTo() {
+	u128 sum{a->lesser + b->lesser, }
+	if constexpr (add_will_overflow(a->lesser, b->lesser)) a->greater++;
+	a->lesser += b->lesser;
+	a->greater += b->greater;
 }
 
 u128* add(u128* a, u128* b) {
@@ -18,6 +48,13 @@ u128* add(u128* a, u128* b) {
 	sum->greater = a->greater + b->greater;
 	if(add_will_overflow(a->lesser, b->lesser)) sum->greater++;
 	return sum;
+}
+
+template<u128 &a, u128 &b>
+constexpr void addTo() {
+	if constexpr (add_will_overflow(a.lesser, b.lesser)) a.greater++;
+	a.lesser += b.lesser;
+	a.greater += b.greater;
 }
 
 u128* subtract(u128* a, u128* b) {
@@ -31,4 +68,23 @@ u128* subtract(u128* a, u128* b) {
 		diff->greater--;
 	}
 	return diff;
+}
+
+constexpr u128 toMid(size_t in) {
+	u128 res{in >> halfsize, in << halfsize};
+	return res;
+}
+
+template<size_t a, size_t b>
+constexpr u128 prod() {
+	constexpr u32_2 A(a);
+	constexpr u32_2 B(b);
+	constexpr u128 prod{A.lesser * B.lesser, A.greater * B.greater};
+	addTo<prod, toMid(A.lesser * B.greater)>();
+	addTo<prod, toMid(A.greater * B.lesser)>();
+	return prod;
+}
+
+int main() {
+	prod<1,2;
 }
